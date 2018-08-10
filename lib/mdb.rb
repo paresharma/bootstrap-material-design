7bb77a2192f193cc5a77f86a45459134e6dfc4d3
paresharma <paresh.brahm@gmail.com>
2018-08-11 00:35:44 +0900

    Turn it into an engine and add to the asset paths

--- lib/mdb.rb
+++ lib/mdb.rb
@@ -1,4 +1,11 @@
 require 'mdb/version'
 
 module Mdb
+  class Engine < ::Rails::Engine
+    initializer 'mdb.assets' do |app|
+      %w(font img js scss).each do |sub|
+        app.config.assets.paths << root.join(sub).to_s
+      end
+    end
+  end
 end
